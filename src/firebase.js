1eb8aa4e2ddf4da1d93c2ef9e7f766a6f6c6faa3
Sungmin Cho <32degree@naver.com>
2025-11-28 05:12:35 +0900

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 // 아까 복사한 설정 코드를 여기에 덮어쓰세요!
 // (apiKey, authDomain, projectId 등이 포함된 객체입니다)
 const firebaseConfig = {
-  apiKey: "AIzaSyD...", 
+  apiKey: "AIzaSyCJy_KNwAia5wXeWfIDYuig7xkbK7x9JWE", 
   authDomain: "my-mini-sns.firebaseapp.com",
   projectId: "my-mini-sns",
-  storageBucket: "my-mini-sns.appspot.com",
-  messagingSenderId: "12345...",
-  appId: "1:12345..."
+  storageBucket: "my-mini-sns2.firebasestorage.app",
+  messagingSenderId: "19426165188",
+  appId: "1:19426165188:web:b15ad9f2bc1dec83ffa491",
+  measurementId: "G-31ZCKNXMLJ"
 };
 
 // 파이어베이스 초기화
